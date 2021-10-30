9b3dc9c260d13b1d66c10488229176007e54b40b
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-10-30 06:49:04 +0000

    説明のコンポーネントを分離

---
+++ app/src/components/Common/DescriptionCard.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+
+export default function DescriptionCard(props) {
+    return (
+        <Card>
+            <CardContent>
+                <Typography variant="h5"
+                    sx={{
+                        fontFamily: 'Zen Kaku Gothic New',
+                        textDecoration: 'underline'
+                    }}>
+                    説明
+                </Typography>
+                <Typography variant="body2" sx={{ margin: 1, overflowWrap: 'break-word', whiteSpace: 'pre-wrap'}}>
+                    {props.description}
+                </Typography>
+            </CardContent>
+        </Card>
+    )
+}
