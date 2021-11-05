e7e33eebe183840cbbbc46eb6c6d609a9834a40f
oq-Yuki-op <o.yukinko0715@gmail.com>
2021-11-05 18:14:33 +0000

    レイアウト修正

--- app/src/components/FaceDetection/FaceDetectionFromImage.js
+++ app/src/components/FaceDetection/FaceDetectionFromImage.js
@@ -8,6 +8,8 @@ import Backdrop from '@mui/material/Backdrop';
 import DescriptionCard from '../Common/DescriptionCard';
 import CroppedFace from './CroppedFace'
 import HiddenInput from '../Common/HiddenInput'
+import Drawer from '@mui/material/Drawer';
+import InfoIcon from '@mui/icons-material/Info';
 import axios from 'axios'
 import { adjustImage } from '../../utils';
 
@@ -21,10 +23,31 @@ export default function FaceDetectionFromImage() {
     const [itemData, SetItemData] = useState(null);
     const [detectedCount, SetDetectedCount] = useState(0);
     const [inferenceTime, SetInferenceTime] = useState(2);
-    const [showResultDetail, setShowResultDetail] = useState(false)
+    const [showResultDetail, setShowResultDetail] = useState(false);
     const inputRef = useRef(null);
 
-    const description = '顔検出機能をお試しできるページです。\nリリースノート'
+    const description = '画像から顔検出機能を利用できるページです。'
+
+    const [state, setState] = React.useState({ left: false });
+
+    const toggleDrawer = (anchor, open) => (event) => {
+        if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+            return;
+        }
+
+        setState({ ...state, [anchor]: open });
+    };
+
+    const list = (anchor) => (
+        <Box
+            sx={{ width: 400 }}
+            role="presentation"
+            onClick={toggleDrawer(anchor, false)}
+            onKeyDown={toggleDrawer(anchor, false)}
+        >
+            <DescriptionCard description={description} />
+        </Box>
+    );
 
     const createImageList = (fileName, bboxes) => {
         var array = [];
@@ -53,7 +76,7 @@ export default function FaceDetectionFromImage() {
                             0, 0, faceRecognitionSideLength, faceRecognitionSideLength);
                         resolveSub(tmpCanvas.toDataURL("image/png"));
                     }).then((val) => {
-                        console.log( item['certainty'])
+                        console.log(item['certainty'])
                         var obj = { title: `${array.length}`, img: val, certainty: item['certainty'] };
                         array.push(obj);
                         resolve();
@@ -100,9 +123,8 @@ export default function FaceDetectionFromImage() {
                 }).then(function (value) {
                     const canvasEle = canvas.current;
                     const json_body = { image: canvasEle.toDataURL("image/png") };
-                    axios.post('/api/face_detection/', json_body)
+                    axios.post('http://localhost:8000/api/face_detection/', json_body)
                         .then(res => {
-                            console.log(res['data']['bboxes'])
                             let bboxes = res['data']['bboxes']
                             SetDetectedCount(res['data']['detected_count'])
                             SetInferenceTime(res['data']['predict_time'])
@@ -133,62 +155,62 @@ export default function FaceDetectionFromImage() {
     };
 
     return (
-        <Box sx={{ display: 'flex', m: 2, boxSizing: 'border-box', minHeight: '90%' }}>
-            <Box sx={{ minWidth: '18%', mr: 2 }}>
-                <DescriptionCard description={description} />
-            </Box>
-            <Box sx={{ minWidth: '80%', display: 'flex', p: 1 }}>
-                <Box sx={{ minWidth: '100%' }}>
+        <Box sx={{ boxSizing: 'border-box', minHeight: '90%' }}>
+            <Box sx={{ p: 1, flexDirection: 'column' }}>
+                <Box sx={{ display: 'flex', justifyContent: 'center' }}>
                     <Backdrop sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }} className={'drop'} open={progress}>
                         <CircularProgress color="primary" size={100} />
                     </Backdrop>
-                    <Box sx={{ display: 'flex' }} >
-                        <Box sx={{ width: '75%', display: 'flex' }}>
-                            <Box sx={{ width: 900 }}>
-                                <canvas ref={canvas} className='canvas' />
-                            </Box>
-                            <Box sx={{ position: 'relative', width: '100%' }}>
-                                <Button
-                                    sx={{ position: 'absolute', bottom: 0, left: 10 }}
-                                    type="submit"
-                                    color="primary"
-                                    variant="contained"
-                                    endIcon={<SendIcon />}
-                                    onClick={() => { inputRef.current.click(); }}>
-                                    画像を送信
-                                </Button>
-                            </Box>
+                    <Box sx={{ display: 'flex', flexDirection: 'column' }}>
+                        <Box>
+                            <React.Fragment key='left'>
+                                <Button onClick={toggleDrawer('left', true)} color="info" startIcon={<InfoIcon />} size='large'>このページについて</Button>
+                                <Drawer anchor='left' open={state['left']} onClose={toggleDrawer('left', false)}>
+                                    {list('left')}
+                                </Drawer>
+                            </React.Fragment>
+                        </Box>
+                        <Box sx={{ position: 'relative' }} m={1}>
+                            <canvas ref={canvas} className='canvas' />
+                            <Button
+                                sx={{ position: 'absolute', bottom: 15, right: 15 }}
+                                type="submit"
+                                color="primary"
+                                variant="contained"
+                                endIcon={<SendIcon />}
+                                onClick={() => { inputRef.current.click(); }}>
+                                画像を送信
+                            </Button>
                             <HiddenInput onFileInputChange={onFileInputChange} ref={inputRef} />
                         </Box>
-                        <Box sx={{ p: 2, border: '1px solid black', borderRadius: 2, minWidth: '35%' }}>
-                            <Typography variant="h5" sx={{ fontFamily: 'Zen Kaku Gothic New', textDecoration: 'underline' }}>
-                                検出結果詳細
-                            </Typography>
-                            {showResultDetail && <React.Fragment>
-                                <Typography variant="h6" sx={{ margin: 1, overflowWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
-                                    検出数：{detectedCount}
-                                </Typography>
-                                <Typography variant="h6" sx={{ margin: 1, overflowWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
-                                    処理時間：{inferenceTime}（秒）
-                                </Typography>
-                            </React.Fragment>
-                            }
-                        </Box>
                     </Box>
-                    <Box sx={{ border: '1px solid black', borderRadius: 2, p: 1, mt: 1, minHeight: 10 }} >
-                        <Typography variant="h6" sx={{ fontFamily: 'Zen Kaku Gothic New', textDecoration: 'underline' }}>
-                            検出画像
+                    <Box sx={{ p: 1, border: '1px solid black', borderRadius: 2, minWidth: '40%' }} m={1}>
+                        <Typography variant="h5" sx={{ fontFamily: 'Zen Kaku Gothic New', textDecoration: 'underline' }}>
+                            検出結果詳細
                         </Typography>
-                        {showResultDetail &&
-                            <Box sx={{ display: 'flex', flexDirection: 'row', p: 1, m: 1, justifyContent: 'flex-start', overflowX: 'scroll' }}>
-                                {itemData?.map((item, index) => (
-                                    <CroppedFace key={index} based64={item.img} index={item.title} certainty={item.certainty} />
-                                ))}
-                            </Box>
+                        {showResultDetail && <React.Fragment>
+                            <Typography variant="h6" sx={{ margin: 1, overflowWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
+                                検出数：{detectedCount}
+                            </Typography>
+                            <Typography variant="h6" sx={{ margin: 1, overflowWrap: 'break-word', whiteSpace: 'pre-wrap' }}>
+                                処理時間：{inferenceTime}（秒）
+                            </Typography>
+                        </React.Fragment>
                         }
                     </Box>
                 </Box>
-
+                <Box sx={{ border: '1px solid black', borderRadius: 2, minHeight: 10 }} mt={2} p={1}>
+                    <Typography variant="h6" sx={{ fontFamily: 'Zen Kaku Gothic New', textDecoration: 'underline' }}>
+                        検出画像
+                    </Typography>
+                    {showResultDetail &&
+                        <Box sx={{ display: 'flex', flexDirection: 'row', p: 1, m: 1, justifyContent: 'flex-start', overflowX: 'scroll' }}>
+                            {itemData?.map((item, index) => (
+                                <CroppedFace key={index} based64={item.img} index={item.title} certainty={item.certainty} />
+                            ))}
+                        </Box>
+                    }
+                </Box>
             </Box>
         </Box>
     )
